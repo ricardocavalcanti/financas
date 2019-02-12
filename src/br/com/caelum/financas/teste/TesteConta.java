615426e99c655df1e6a82fd631f0ec74fd71201c
ricardo.cavalcanti <ricardo.cavalcanti@wrodriguespw108.demprel.local>
2019-02-12 15:48:32 -0300

    Melhorando o código e populando o banco.

--- src/br/com/caelum/financas/teste/TesteConta.java
+++ src/br/com/caelum/financas/teste/TesteConta.java
@@ -1,30 +1,32 @@
 package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteConta {
 
 	public static void main(String[] args) {
 
 		Conta conta = new Conta();
-		conta.setTitular("Leonardo");
-		conta.setAgencia("123");
+		conta.setTitular("Danilo");
+		conta.setAgencia("Banco do Brasil");
 		conta.setBanco("Caixa Economica");
 		conta.setNumero("456");
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = new JPAUtil().getEntityManager();
+		
 
 		em.getTransaction().begin();
 		em.persist(conta);
+		
+		conta.setBanco("Bradesco");
+		
+		
 		em.getTransaction().commit();
 
 		em.close();
-		emf.close();
+		
 
 	}
 
