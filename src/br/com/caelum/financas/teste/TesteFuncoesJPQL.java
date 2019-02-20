211fcd329ef16602f1859ad72089aa1d14233db2
ricardo.cavalcanti <ricardo.cavalcanti@wrodriguespw108.demprel.local>
2019-02-20 15:59:20 -0300

    Organizando o código

---
+++ src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -0,0 +1,36 @@
+package br.com.caelum.financas.teste;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+
+import br.caelum.financas.dao.MovimentacaoDao;
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteFuncoesJPQL {
+
+	public static void main(String args[]) {
+
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+
+		Conta conta = new Conta();
+		conta.setId(2);
+
+		MovimentacaoDao dao = new MovimentacaoDao(em);
+		
+		List<Double> medias = dao.getMediasPorDiaETipo(TipoMovimentacao.SAIDA, conta);
+
+		for (Double media : medias) {
+
+			System.out.println("A média é: " + media);
+
+		}
+
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+}
--- src/br/com/caelum/financas/teste/TesteTodasMovimentacoesContas.java
+++ src/br/com/caelum/financas/teste/TesteTodasMovimentacoesContas.java
@@ -11,27 +11,25 @@ import br.com.caelum.financas.util.JPAUtil;
 public class TesteTodasMovimentacoesContas {
 
 	public static void main(String args[]) {
-		
+
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
-		
+
 		String jpql = "select distinct c from Conta c left join fetch c.movimentacoes";
-		
+
 		Query query = em.createQuery(jpql);
-		
-		
+
 		List<Conta> todasAsContas = query.getResultList();
-		
+
 		for (Conta conta : todasAsContas) {
-			
+
 			System.out.println("");
 			System.out.println("Titular: " + conta.getTitular());
 			System.out.println("Movimentacoes: ");
 			System.out.println("");
 			System.out.println(conta.getMovimentacao());
-			
+
 		}
-		
 
 	}
 
