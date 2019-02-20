73888207edd9c59f4d95a98b387ea0bd17d5c645
ricardo.cavalcanti <ricardo.cavalcanti@wrodriguespw108.demprel.local>
2019-02-20 16:21:54 -0300

    Usando NamedQueries

--- src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -2,8 +2,8 @@ package br.com.caelum.financas.teste;
 
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
-import br.caelum.financas.dao.MovimentacaoDao;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
@@ -18,9 +18,13 @@ public class TesteFuncoesJPQL {
 		Conta conta = new Conta();
 		conta.setId(2);
 
-		MovimentacaoDao dao = new MovimentacaoDao(em);
+	   TypedQuery<Double> typedQuery =  em.createNamedQuery("MediasPorDiaETipo", Double.class);
+	   
+	   typedQuery.setParameter("pConta", conta);
+	   typedQuery.setParameter("pTipo", TipoMovimentacao.SAIDA);		
 		
-		List<Double> medias = dao.getMediasPorDiaETipo(TipoMovimentacao.SAIDA, conta);
+		
+	   List<Double> medias = typedQuery.getResultList();
 
 		for (Double media : medias) {
 
